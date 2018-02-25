eccb0dd871598e178395be3263fe0057780b4da6
chrissou <nicolchristelle@gmail.com>
2018-02-25 23:14:51 +0100

    mobil-dekstop

--- js/app.js
+++ js/app.js
@@ -1,9 +1,23 @@
-// Application
-var app = {
+var app ={
   init: function() {
     console.log('App : init');
-  },
-};
 
+$(window).scroll(function(){
+  posScroll = $(document).scrollTop();
+          if(posScroll >=69)
+          $('.nav').slideDown(450);
+          if(posScroll >=69){
+
+            $(".fixe-nav").hide();
+          }
+        else
+          $('.nav').slideUp(450)&&
+            $(".fixe-nav").show(450)
+
+
+});
+}
+
+};
 // quand le DOM est prêt, on lance app.init
 $(app.init);
